6a7ee02ae0b62c34c9689df346c9f0eff2d861e8
DancheBacheva <danchebacheva@gmail.com>
2023-07-09 23:07:41 +0200

    domasna2

---
+++ Domasna2/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const bookController = require("./controller/bookController")
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://danchebacheva:5B1keENwdr50XhWm@cluster0.hpujfqj.mongodb.net/db_books?retryWrites=true&w=majority",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+  console.log("Successful connection!");
+}).catch((err)=>{
+  console.log(err);
+})
+
+app.post("/books", bookController.createBook);
+app.get("/books", bookController.getAllBooks);
+
+app.listen(10000, ()=>{
+  console.log("The application is running")
+});
---
+++ Domasna2/controller/bookController.js
@@ -0,0 +1,40 @@
+const Book = require("../model/bookModel");
+
+exports.createBook = async (req, res) => {
+  try{
+    const newBook = await Book.create(req.body);
+      res.status(201).json({
+        status: "success",
+        data: {
+          book: newBook,
+      },
+    });
+    
+    res.send(newBook);
+  }catch(err){
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getAllBooks = async (req, res) => {
+  try{
+    const books = await Book.find();
+
+    res.status(200).json({
+      test: "Test",
+      status: "Success",
+      data: {
+        books: books,
+      },
+    });
+
+  }catch(err){
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
